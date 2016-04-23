3e8ad5be47d73524f0d32a4e94029c77254637b7
Kevin van Dijk <kevin@edgybits.com>
2016-04-23 02:45:39 +0200

    Add upload form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
-export default function () {
-  return (
-    <div>Hello World</div>
-  );
+class App extends React.Component {
+  state = {
+    files: null,
+  };
+
+  onDrop = (files) => {
+    const prevFiles = this.state.files || [];
+    this.setState({
+      files: prevFiles.concat(files),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Dropzone onDrop={ this.onDrop }>
+          <div>Drop a video to upload</div>
+        </Dropzone>
+        { this.state.files &&
+          <div>
+            <h2>Files:</h2>
+            <ul>
+              { this.state.files.map(file => <li>{ file.name }</li>) }
+            </ul>
+            <button onClick={ this.onSubmit }>Upload</button>
+          </div>
+        }
+      </div>
+    );
+  }
 }
+
+export default App;
