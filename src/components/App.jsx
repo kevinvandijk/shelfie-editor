d0c3871012ceaad65dbf3572755f4c65300b21cb
Kevin van Dijk <kevin@edgybits.com>
2016-05-01 01:35:44 +0200

    Split up Upload and File List parts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import VideoCropper from './VideoCropper';
 
 export default class App extends React.Component {
   state = {
+    page: 'upload',
     videoUrl: null,
     videoName: null,
   };
@@ -13,16 +14,36 @@ export default class App extends React.Component {
     this.setState({
       videoUrl: `http://127.0.0.1:3000/uploads/${encodeURIComponent(file)}`,
       videoName: file,
+      page: 'video',
     });
   }
 
   render() {
-    console.log('render', this.state.videoUrl);
+    const { page } = this.state;
+
     return (
       <div>
-        <UploadForm />
-        <FileList onFileClick={ this.openFile } />
-        { this.state.videoUrl
+        <nav className="navbar navbar-default">
+          <ul className="nav navbar-nav">
+            <li><a href="#" onClick={ () => this.setState({ page: 'upload' }) }>Upload</a></li>
+            <li><a href="#" onClick={ () => this.setState({ page: 'files' }) }>File list</a></li>
+          </ul>
+        </nav>
+        { page === 'upload' &&
+          <div className="container">
+            <h2>Upload</h2>
+            <UploadForm />
+          </div>
+        }
+
+        { page === 'files' &&
+          <div className="container">
+            <h2>Files</h2>
+            <FileList onFileClick={ this.openFile } />
+          </div>
+        }
+
+        { page === 'video' && this.state.videoUrl
           ? <VideoCropper name={ this.state.videoName } url={ this.state.videoUrl } />
           : null
         }
