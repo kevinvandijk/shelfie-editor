c7c334026ab9c3fe92996c6f4a78f23e5906f748
Kevin van Dijk <kevin@edgybits.com>
2016-05-01 01:17:11 +0200

    Add ability to cut video and return output file location

--- api/server.js
+++ api/server.js
@@ -7,6 +7,10 @@ import convert from 'koa-convert';
 import multer from 'koa-multer';
 import bodyParser from 'koa-bodyparser';
 import fs from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
+import uuid from 'node-uuid';
+import moment from 'moment';
+import 'moment-duration-format';
 
 const router = new KoaRouter();
 const app = new Koa();
@@ -25,9 +29,27 @@ router.get('/list', ctx => {
   ctx.body = fs.readdirSync('./uploads');
 });
 
-router.post('/cut', ctx => {
-  ctx.body = ctx.request.body;
-});
+router.post('/cut', ctx => new Promise(resolve => {
+  const body = ctx.request.body;
+  const cutStart = body.cutStart;
+  const startTime = moment.duration(cutStart, 'seconds').format('hh:mm:ss', { trim: false });
+  const duration = moment.duration((body.cutEnd - cutStart) * 1000).asSeconds();
+  const extension = body.file.split('.').pop();
+  const name = `${uuid.v4()}.${extension}`;
+
+  ffmpeg(`./uploads/${body.file}`)
+    .setStartTime(startTime)
+    .setDuration(duration)
+    .output(`./uploads/${name}`)
+    .on('end', () => {
+      ctx.body = {
+        output: `./uploads/${name}`,
+      };
+
+      resolve(ctx);
+    })
+    .run();
+}));
 
 const staticServer = serve('./uploads');
 
